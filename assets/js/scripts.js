09e992ef42c156f80a8b06212885ef94feb18e1e
Wiviart <Wisiviart@gmail.com>
2024-12-21 20:38:55 +0700

    delete redundant

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,26 @@ loadExternalScripts();
 
 // Optionally, log the photoGalleries array to the console to check if it's populated correctly
 console.log(photoGalleries);
+
+// Function to add event to Google Calendar
+function addEventToGoogleCalendar(eventName, eventDescription, eventLocation, eventStartDate, eventEndDate) {
+    const googleCalendarUrl = `https://calendar.google.com/calendar/r/eventedit?text=${encodeURIComponent(eventName)}&details=${encodeURIComponent(eventDescription)}&location=${encodeURIComponent(eventLocation)}&dates=${eventStartDate.replace(/[-:]/g, '')}Z/${eventEndDate.replace(/[-:]/g, '')}Z`;
+    window.open(googleCalendarUrl, '_blank');
+}
+
+// Add click event listeners to all calendar buttons
+document.querySelectorAll('.calendar-button-custom-click').forEach(button => {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Extract event details from data attributes
+        const eventName = this.getAttribute('data-name');
+        const eventDescription = this.getAttribute('data-description');
+        const eventLocation = this.getAttribute('data-location');
+        const eventStartDate = this.getAttribute('data-start');
+        const eventEndDate = this.getAttribute('data-end');
+
+        // Call the function to add the event
+        addEventToGoogleCalendar(eventName, eventDescription, eventLocation, eventStartDate, eventEndDate);
+    });
+});
