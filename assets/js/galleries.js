81a0e7051284314789b4d00d0e0bf81a550b3dc4
Wiviart <Wisiviart@gmail.com>
2024-12-27 06:29:06 +0700

    update

--- assets/js/galleries.js
+++ assets/js/galleries.js
@@ -7,11 +7,6 @@ photoGalleries.push({
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu là điều kiện trong đó hạnh phúc của người khác là điều cần thiết cho chính bạn</h4></div>`
 });
 photoGalleries.push({
-    src: "./assets/imgs/6048.jpg",
-    thumb: "./assets/imgs/6048.jpg",
-    subHtml: `<div class="lg-sub-html"><h4>Tình yêu không chỉ là một danh từ - nó là một động từ; nó còn hơn cả một cảm giác - đó là sự quan tâm, chia sẻ, giúp đỡ, hy sinh</h4></div>`
-});
-photoGalleries.push({
     src: "./assets/imgs/4988.jpg",
     thumb: "./assets/imgs/4988.jpg",
     subHtml: `<div class="lg-sub-html"><h4>Vì vậy, để được ai đó nhìn thấy đầy đủ và được yêu mến dù thế nào đi nữa — đây là một sự dâng hiến của con người có thể là điều kỳ diệu</h4></div>`
@@ -27,6 +22,11 @@ photoGalleries.push({
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không có rào cản. Nó nhảy rào, nhảy rào, xuyên tường để đến đích với đầy hy vọng</h4></div>`
 });
 photoGalleries.push({
+    src: "./assets/imgs/6048.jpg",
+    thumb: "./assets/imgs/6048.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Tình yêu không chỉ là một danh từ - nó là một động từ; nó còn hơn cả một cảm giác - đó là sự quan tâm, chia sẻ, giúp đỡ, hy sinh</h4></div>`
+});
+photoGalleries.push({
     src: "./assets/imgs/6058.jpg",
     thumb: "./assets/imgs/6058.jpg",
     subHtml: `<div class="lg-sub-html"><h4>Tôi có thể chinh phục thế giới bằng một tay miễn là bạn đang nắm tay kia</h4></div>`
@@ -36,26 +36,81 @@ photoGalleries.push({
     thumb: "./assets/imgs/6060.jpg",
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu đích thực đứng về phía nhau trong những ngày tốt đẹp và sát cánh hơn trong những ngày tồi tệ</h4></div>`
 });
-// photoGalleries.push({
-//     src: "",
-//     thumb: "",
-//     subHtml: `<div class="lg-sub-html"><h4>Được ai đó yêu sâu sắc sẽ mang lại cho bạn sức mạnh, trong khi yêu ai đó sâu sắc sẽ cho bạn dũng khí</h4></div>`
-// });
-// photoGalleries.push({
-//     src: "",
-//     thumb: "",
-//     subHtml: `<div class="lg-sub-html"><h4>Đối với thế giới, bạn có thể là một người, nhưng với một người, bạn là cả thế giới</h4></div>`
-// });
-// photoGalleries.push({
-//     src: "",
-//     thumb: "https://cdn.biihappy.com/ziiweb/default/website/galleries/619902821ca1d308fe5da283/small.jpg",
-//     subHtml: `<div class="lg-sub-html"><h4>Tôi yêu bạn vì tất cả những gì bạn đang có, tất cả những gì bạn đã có, và tất cả những gì bạn chưa hiện hữu</h4></div>`
-// });
-// photoGalleries.push({
-//     src: "https://cdn.biihappy.com/ziiweb/default/website/galleries/6199027ed7e97a1853119578/large.jpg",
-//     thumb: "https://cdn.biihappy.com/ziiweb/default/website/galleries/6199027ed7e97a1853119578/small.jpg",
-//     subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
-// });
+photoGalleries.push({
+    src: "./assets/imgs/7634.jpg",
+    thumb: "./assets/imgs/7634.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Được ai đó yêu sâu sắc sẽ mang lại cho bạn sức mạnh, trong khi yêu ai đó sâu sắc sẽ cho bạn dũng khí</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7635.jpg",
+    thumb: "./assets/imgs/7635.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Đối với thế giới, bạn có thể là một người, nhưng với một người, bạn là cả thế giới</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7636.jpg",
+    thumb: "./assets/imgs/7636.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Tôi yêu bạn vì tất cả những gì bạn đang có, tất cả những gì bạn đã có, và tất cả những gì bạn chưa hiện hữu</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7637.jpg",
+    thumb: "./assets/imgs/7637.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7638.jpg",
+    thumb: "./assets/imgs/7638.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7639.jpg",
+    thumb: "./assets/imgs/7639.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7640.jpg",
+    thumb: "./assets/imgs/7640.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7641.jpg",
+    thumb: "./assets/imgs/7641.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7642.jpg",
+    thumb: "./assets/imgs/7642.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7643.jpg",
+    thumb: "./assets/imgs/7643.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7644.jpg",
+    thumb: "./assets/imgs/7644.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7645.jpg",
+    thumb: "./assets/imgs/7645.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7646.jpg",
+    thumb: "./assets/imgs/7646.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7647.jpg",
+    thumb: "./assets/imgs/7647.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
+photoGalleries.push({
+    src: "./assets/imgs/7648.jpg",
+    thumb: "./assets/imgs/7648.jpg",
+    subHtml: `<div class="lg-sub-html"><h4>Bước đi với bàn tay của bạn trong tay tôi và bàn tay của tôi trong tay bạn, đó chính xác là nơi tôi muốn luôn ở đó</h4></div>`
+});
 
 // Optionally, log the photoGalleries array to the console to check if it's populated correctly
 // console.log(photoGalleries);
