d67c55a5f04262230fc35dc51dd30c889157e171
Wiviart <Wisiviart@gmail.com>
2025-01-10 08:16:20 +0700

    server update

--- proxy_server/server.js
+++ proxy_server/server.js
@@ -28,3 +28,19 @@ app.post("/proxy", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("Proxy running on http://localhost:3000"));
+
+const getURL = (sheetName) => `https://script.google.com/macros/s/AKfycbwo_VSiKLjq84y0wYroDaQUiJktOIZ4-cZeZQyoId0PXAxJAT_28uTVao5lpW0neS0B/exec?sheetName=${sheetName}`;
+
+app.get("/proxy", async (req, res) => {
+    try {
+        const sheetName = req.query.sheetName;
+        const url = getURL(sheetName);
+        const response = await fetch(url);
+        console.log("Response Status:", response.status);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Failed to fetch data from Google Apps Script" });
+    }
+});
