63100c8eca1ec980680dec47bb0e10b378ca1949
Wiviart <Wisiviart@gmail.com>
2025-01-12 17:58:53 +0700

    update

--- assets/js/musicplay.js
+++ assets/js/musicplay.js
@@ -1,16 +1,15 @@
-
 function playPause() {
     const volumeOff = document.getElementById('playerVolumeOff');
     const volumeOn = document.getElementById('playerVolumeOn');
 
     // Toggle play/pause functionality
-    if (volumeOff.style.display === 'block') {
-        volumeOff.style.display = 'none';
-        volumeOn.style.display = 'block';
-        // Add code to play music here
-    } else {
+    if (volumeOff.style.display === 'none') {
         volumeOff.style.display = 'block';
         volumeOn.style.display = 'none';
         // Add code to pause music here
+    } else {
+        volumeOff.style.display = 'none';
+        volumeOn.style.display = 'block';
+        // Add code to play music here
     }
 }
